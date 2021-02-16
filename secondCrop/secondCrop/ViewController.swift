ad66e5fcdcfc1fe07905cb0d0ea5e683035c355a
邓江洲 <dengjiangzhouReal@gmail.com>
2021-02-16 23:01:16 +0800

    优化

--- secondCrop/secondCrop/ViewController.swift
+++ secondCrop/secondCrop/ViewController.swift
@@ -11,13 +11,10 @@ class ViewController: UIViewController {
     
     let cropView = SECropView()
     @IBOutlet weak var imageView: UIImageView!
-
-    var b = CGSize.zero
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        b = imageView.bounds.size
         cropView.configureWithCorners(on: imageView)
     }
 
@@ -25,13 +22,10 @@ class ViewController: UIViewController {
     
     @IBAction func saveImg(_ sender: Any) {
         do {
-            guard let corners = cropView.cornerLocations else { return }
-            guard let image = imageView.image else { return }
-            let cn = corners.map { (pt) -> CGPoint in
-                return pt.inner(img: b, relative: image.size)
+            guard let corners = cropView.cornerLocations, let image = imageView.image else {
+                return
             }
-            let croppedImage = try SEQuadrangleHelper.cropImage(with: image, quad: cn)
-            
+            let croppedImage = try SEQuadrangleHelper.cropImage(with: image, quad: corners)
             performSegue(withIdentifier: "doCrop", sender: croppedImage)
         } catch let error as SECropError {
             print(error)
@@ -48,11 +42,3 @@ class ViewController: UIViewController {
 }
 
 
-
-extension CGPoint{
-    func inner(img s: CGSize, relative dot: CGSize) -> CGPoint{
-        let xx = x * dot.width/s.width
-        let yy = y * dot.height/s.height
-        return CGPoint(x: xx, y: yy)
-    }
-}
