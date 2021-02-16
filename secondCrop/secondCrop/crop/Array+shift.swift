b92ad517c89dc25e375dacf7a482be6a905a69ed
邓江洲 <dengjiangzhouReal@gmail.com>
2021-02-17 00:23:20 +0800

    搞笑 代码

--- secondCrop/secondCrop/crop/Array+shift.swift
+++ secondCrop/secondCrop/crop/Array+shift.swift
@@ -9,13 +9,23 @@ import Foundation
 
 extension Array {
     func shifted(by shiftAmount: Int) -> [Element]{
-        guard self.count > 0, (shiftAmount % self.count) != 0 else { return self }
-        let moduloShiftAmount = shiftAmount % self.count
-        
-        let negativeShift = shiftAmount < 0
-        let effectiveShiftAmount = negativeShift ? moduloShiftAmount + self.count : moduloShiftAmount
-        let shift: (Int) -> Int = { return $0 + effectiveShiftAmount >= self.count ? $0 + effectiveShiftAmount - self.count : $0 + effectiveShiftAmount }
-        
-        return self.enumerated().sorted(by: { shift($0.offset) < shift($1.offset) }).map { $0.element }
+        guard count > 0, (shiftAmount % count) != 0 else { return self }
+        let moduloShiftAmount = shiftAmount % count
+        let effectiveShiftAmount: Int
+        if shiftAmount < 0{
+            effectiveShiftAmount = moduloShiftAmount + count
+        }
+        else{
+            effectiveShiftAmount = moduloShiftAmount
+        }
+        let shift: (Int) -> Int = {
+            if $0 + effectiveShiftAmount >= count{
+                return $0 + effectiveShiftAmount - count
+            }
+            else{
+                return $0 + effectiveShiftAmount
+            }
+        }
+        return enumerated().sorted(by: { shift($0.offset) < shift($1.offset) }).map { $0.element }
     }
 }
